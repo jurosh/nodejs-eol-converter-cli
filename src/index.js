ae4e1af7c39c76ca2e37e89167f1f6e821699d7b
Ivan Milosavljevic (TheJavaGuy) <TheJavaGuy@yandex.com>
2024-07-31 11:09:21 +0200

    update glob to 9.3.5 because 7.x isn't supported anymore

--- src/index.js
+++ src/index.js
@@ -6,25 +6,22 @@ const glob = require('glob')
 const fs = require('fs')
 const path = require('path')
 
-const argv = process.argv
-const inputFilesGlob = argv[argv.length - 1]
-const isWarmup = argv[argv.length - 2] === 'warmup'
-const isCrlf = argv[argv.length - 2] === 'crlf'
+async function run() {
+  const argv = process.argv
+  const inputFilesGlob = argv[argv.length - 1]
+  const isWarmup = argv[argv.length - 2] === 'warmup'
+  const isCrlf = argv[argv.length - 2] === 'crlf'
 
-const dir = process.cwd()
+  const dir = process.cwd()
 
-console.log('Running in directory ' + dir)
-console.log('Files GLOB regex: ' + inputFilesGlob)
-console.log(isWarmup
-  ? 'WARMUP: will only list files, no action will be performed'
-  : 'Converting to ' + (isCrlf ? 'CRLF' : 'LF'))
-console.log('---')
+  console.log('Running in directory ' + dir)
+  console.log('Files GLOB regex: ' + inputFilesGlob)
+  console.log(isWarmup
+    ? 'WARMUP: will only list files, no action will be performed'
+    : 'Converting to ' + (isCrlf ? 'CRLF' : 'LF'))
+  console.log('---')
 
-glob(inputFilesGlob, { nodir: true }, function (error, files) {
-  if (error) {
-    console.error(error)
-    return
-  }
+  const files = await glob(inputFilesGlob, { nodir: true })
   if (!files || files.length === 0) {
     return console.error('ERROR: no files found')
   }
@@ -42,4 +39,6 @@ glob(inputFilesGlob, { nodir: true }, function (error, files) {
     }
   })
   console.log('---')
-})
+}
+
+run()
